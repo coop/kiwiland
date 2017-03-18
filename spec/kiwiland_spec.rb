2cb963c69f4640087356122df6c4757b84f6276a
Tim Cooper <tim@timcorp.io>
2017-03-18 21:01:51 +1000

    Document public methods for Graph

--- spec/kiwiland_spec.rb
+++ spec/kiwiland_spec.rb
@@ -9,10 +9,20 @@ RSpec.describe Kiwiland do
       @graph = Hash.new { |h, k| h[k] = {} }
     end
 
+    # Add edges to describe the directed graph.
+    #
+    # @param source [String] the source node
+    # @param terminal [String] the terminal node
+    # @param weight [Integer] the distance between source and terminal
+    # @return [nil]
     def add_edge(source:, terminal:, weight:)
       @graph[source][terminal] = weight
     end
 
+    # Calculate the distance for a given route.
+    #
+    # @param nodes [Array<String>]
+    # @return [Integer, NoRouteExists] the distance for a given route
     def distance_between(*nodes)
       nodes.each_cons(2).inject(0) do |acc, (current_node, next_node)|
         if (weight = @graph[current_node][next_node])
@@ -23,6 +33,15 @@ RSpec.describe Kiwiland do
       end
     end
 
+    # Calculate the number of routes betwen two nodes with a given number of
+    # stops.
+    #
+    # @param source [String] the source node
+    # @param terminal [String] the terminal node
+    # @param exact_stops [Integer, nil] the expect number of stops
+    # @param max_stops [Integer] the maximum number of stops - defaults to
+    #   `exact_stops`
+    # @return [Integer] the number of routes between two nodes
     def number_of_trips(source:, terminal:, exact_stops: nil, max_stops: exact_stops, depth: 0)
       return 0 if depth > max_stops
       if exact_stops
@@ -42,6 +61,16 @@ RSpec.describe Kiwiland do
       end
     end
 
+    # Calculate the number of routes within a specified distance between two
+    # nodes.
+    #
+    # @param source [String] the source node
+    # @param terminal [String] the terminal node
+    # @param max_distance [Integer] the maximum distance to travel between
+    #   source and terminal
+    # @param current_distance [Integer] the current accumlative distance between
+    #   source and terminal
+    # @return [Integer] the number of routes within a specified distance
     def number_of_routes_within(source:, terminal:, max_distance:, current_distance: 0)
       return 0 if current_distance >= max_distance
 
